d183a5f00bd7342525f3610728aac7684a01b806
Iksi <jur@iksi.cc>
2015-01-05 18:17:33 +0100

    Update embed.php

--- tags/embed.php
+++ tags/embed.php
@@ -6,12 +6,10 @@ kirbytext::$tags['embed'] = array(
         'class'
     ),
     'html' => function($tag) {
-
-        return '<div class="' . $tag->attr('class', kirby()->option('kirbytext.embed.class', 'embed')) . ' js-embed" data-url="' . $tag->attr('embed') .'" data-autoplay="' . $tag->attr('autoplay', 'false') . '">'
-             . '<a href="' . $tag->attr('embed') . '">'
-             . parse_url($tag->attr('embed'), PHP_URL_HOST) . parse_url($tag->attr('embed'), PHP_URL_PATH)
-             . '</a>'
-             . '</div>';
-
+        return embed(
+            $tag->attr('embed'),
+            $tag->attr('autoplay'),
+            $tag->attr('class', kirby()->option('kirbytext.embed.class', 'embed'))
+        );
     }
 );
