42295ea99265da0200d783d0a6dad17b656fc7db
Iksi <jur@iksi.cc>
2015-01-05 19:26:31 +0100

    Update embed.php

--- plugins/embed.php
+++ plugins/embed.php
@@ -7,11 +7,11 @@
  * @version 1.1.0
  */
 
-function embed($url, $autoplay = NULL, $class)
+function embed($url, $autoplay = NULL, $class = NULL)
 {
     $data = array(
         'url'      => $url,
-        'class'    => $class,
+        'class'    => is_null($class) ? kirby()->option('kirbytext.embed.class', 'embed') : $class,
         'label'    => preg_replace('/^https?:\/\//i', '', $url),
         'autoplay' => $autoplay
     );
