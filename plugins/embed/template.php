db386c18ffc92012c04255a8ae488c6579514a41
Iksi <jur@iksi.cc>
2015-01-06 01:56:02 +0100

    removed data-url attribute

--- plugins/embed/template.php
+++ plugins/embed/template.php
@@ -1,3 +1,3 @@
-<div class="<?php echo $class ?> js-embed" data-url="<?php echo $url ?>" data-autoplay="<?php echo $autoplay ?>">
+<div class="<?php echo $class ?> js-embed" data-autoplay="<?php echo $autoplay ?>">
     <a href="<?php echo $url ?>" rel="external"><?php echo $label ?></a>
 </div>
