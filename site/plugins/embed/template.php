18fbd3090104f06d5cf125a99a46cf665ac704a8
iksi <jur@iksi.cc>
2015-02-05 16:53:46 +0100

    Update template.php

--- site/plugins/embed/template.php
+++ site/plugins/embed/template.php
@@ -1,3 +1,9 @@
 <div class="<?php echo $class ?> js-embed">
-    <a href="<?php echo $url ?>" rel="external"><?php echo $placeholder ?></a>
+    <a class="<?php echo $class ?>-link" href="<?php echo $url ?>" rel="external">
+        <?php if ($image !== false): ?>
+        <img class="<?php echo $class ?>-image" src="<?php echo $image ?>" alt="<?php echo $text ?>">
+        <?php else: ?>
+        <span class="<?php echo $class ?>-text"><?php echo $text ?></span>
+        <?php endif ?>
+    </a>
 </div>
