e09c0fb0e3ad3ce3e407782a8952c3f348f9d745
iksi <jur@iksi.cc>
2015-02-05 20:45:05 +0100

    Update embed.php

--- site/plugins/embed/embed.php
+++ site/plugins/embed/embed.php
@@ -66,7 +66,7 @@ kirbytext::$tags['embed'] = array(
 
         return embed($tag->attr('embed'))->html(array_filter(array(
             'text'  => $tag->attr('text'),
-            'image' => $image ? $image->url() : url($tag->attr('image')
+            'image' => $image ? $image->url() : $tag->attr('image')
         )));
     }
 );
