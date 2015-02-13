e5826e20a8aa51fa96393c2acac0f475e1d48c6a
Iksi <jur@iksi.cc>
2015-02-13 15:13:51 +0100

    updated oembed route

--- site/config/config.php
+++ site/config/config.php
@@ -1,13 +1,15 @@
 <?php
 
+// Set the class for embeds
 c::set('embed.class', 'embed');
 
+// Route for oembed request
 c::set('routes', array(
     array(
-        'pattern' => 'embed',
-        'action' => function() {
+        'pattern' => 'oembed',
+        'action'  => function() {
             return response::json(
-                embed(get('url'))->fetch()
+                oembed(get('url'))
             );
         }
     )
